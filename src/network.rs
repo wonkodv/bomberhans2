f8f40fd2c372abba15127d7777537d390eb0dda7
Wonko <wonko@hanstool.org>
2022-12-28 21:30:45 +0100

    net: header struct

---
+++ src/network.rs
@@ -0,0 +1,21 @@
+const BOMBERHANS_MAGIC_NO_V1: u32 = 0x1f4a3__001; // 💣
+
+
+struct Header {
+    magic: u32,
+    sequence: u32,
+    ack: u32,
+    older_acks: u32,
+}
+
+impl Header {
+    fn new(sequence: u32, ack: u32, older_acks: u32) -> Self {
+        Self {
+            magic: BOMBERHANS_MAGIC_NO_V1,
+            sequence,
+            ack,
+            older_acks,
+        }
+    }
+}
+
