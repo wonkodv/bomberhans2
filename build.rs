7364f04b50e7d6a16156fc0394a1e31d010ee4c6
Wonko <wonko@hanstool.org>
2023-12-09 00:27:33 +0100

    bump version

--- build.rs
+++ build.rs
@@ -3,15 +3,17 @@ fn main() {
     let output = Command::new("git")
         .arg("describe")
         .arg("--tags")
+        .arg("--match")
+        .arg("v*")
         .output()
         .expect("git describe works");
-    let git_version = String::from_utf8(output.stdout).expect("git dewcribe gives utf8");
+    let git_version = String::from_utf8(output.stdout).expect("git describe gives utf8");
+    let git_version = git_version.trim();
 
     let cargo_version = concat!("v", env!("CARGO_PKG_VERSION"));
-    if !git_version.starts_with(cargo_version) {
-        panic!(
-            "expected git-version {git_version:?} to start with cargo-version {cargo_version:?}"
-        );
-    }
-    println!("cargo:rustc-env=VERSION={}", git_version);
+    assert!(
+        git_version.starts_with(cargo_version),
+        "expected git-version {git_version:?} to start with cargo-version {cargo_version:?}"
+    );
+    println!("cargo:rustc-env=VERSION={git_version}");
 }
